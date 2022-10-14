416a370b2be93c7359557f5b44656887ac3f3232
Angel-88 <dasha.zagrebelna88@gmail.com>
2022-10-15 00:10:25 +0200

    add first lab

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+    var count = 0
+    private lateinit var viewModel:MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        val textView = findViewById<TextView>(R.id.text_id)
+        val button = findViewById<Button>(R.id.button_id)
+
+        textView.text = viewModel.count.toString()
+
+        button.setOnClickListener {
+            viewModel.updateCount()
+            textView.text = viewModel.count.toString()
+        }
     }
 }
